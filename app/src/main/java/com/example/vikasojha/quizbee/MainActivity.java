b97f2faad62d090ea77da06c4315d7da557abad7
Sambit Kumar Pradhan <sambitkumarpradhan8@gmail.com>
2021-02-13 19:39:08 +0530

    First quizz app commit

--- app/src/main/java/com/example/vikasojha/quizbee/MainActivity.java
+++ app/src/main/java/com/example/vikasojha/quizbee/MainActivity.java
@@ -30,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-                aboutbutton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                    Intent intent=new Intent(getApplicationContext(),DeveloperActivity.class);
-                    startActivity(intent);
-            }
-        });
+////                aboutbutton.setOnClickListener(new View.OnClickListener() {
+////                    @Override
+////                    public void onClick(View v) {
+////                    Intent intent=new Intent(getApplicationContext(),DeveloperActivity.class);
+////                    startActivity(intent);
+//            }
+//        });
     }
 }
